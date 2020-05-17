914424ab2e5d3d48d53b44996a4a3c87f7bac41d
HouariZegai <houarizegai14@gmail.com>
2020-05-17 05:05:10 +0100

    Remove Camera dependency & test front-end

--- src/main/java/com/houarizegai/facedetection/App.java
+++ src/main/java/com/houarizegai/facedetection/App.java
@@ -1,19 +1,38 @@
 package com.houarizegai.facedetection;
 
-import com.houarizegai.facedetection.engine.FaceDetectionEngine;
-import com.houarizegai.facedetection.model.DetectedFace;
+import com.houarizegai.facedetection.controllers.FaceDetectionController;
 import javafx.application.Application;
-import javafx.scene.image.Image;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+import java.io.IOException;
 
 public class App extends Application {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
     @Override
     public void start(Stage stage) {
-        DetectedFace detectedFace = FaceDetectionEngine.start(new Image("/images/input.jpg"));
-        System.out.println(detectedFace.getNbrOfFaces());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/FaceDetection.fxml"));
+            stage.setScene(new Scene(root));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        stage.setTitle("Face Detection");
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() {
+        FaceDetectionController.capture.release();
     }
 }

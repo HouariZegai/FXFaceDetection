217cbb25b72a0f7431dee1e6a8e16145390a2917
HouariZegai <houarizegai14@gmail.com>
2020-05-17 02:35:08 +0100

    Added helpful functions for face detection

---
+++ src/main/java/com/houarizegai/facedetection/utils/Utils.java
@@ -0,0 +1,54 @@
+package com.houarizegai.facedetection.utils;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritablePixelFormat;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.nio.ByteBuffer;
+
+public class Utils {
+    public static Image matToImage(Mat frame) {
+        try {
+            return SwingFXUtils.toFXImage(matToBufferedImage(frame), null);
+        } catch (Exception e) {
+            System.err.println("Cannot convert the Mat object: " + e);
+            return null;
+        }
+    }
+
+    public static Mat imageToMat(Image image) {
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        byte[] buffer = new byte[width * height * 4];
+
+        PixelReader reader = image.getPixelReader();
+        WritablePixelFormat<ByteBuffer> format = WritablePixelFormat.getByteBgraInstance();
+        reader.getPixels(0, 0, width, height, format, buffer, 0, width * 4);
+
+        Mat mat = new Mat(height, width, CvType.CV_8UC4);
+        mat.put(0, 0, buffer);
+        return mat;
+    }
+
+    private static BufferedImage matToBufferedImage(Mat original) {
+        int width = original.width(), height = original.height(), channels = original.channels();
+        byte[] sourcePixels = new byte[width * height * channels];
+        original.get(0, 0, sourcePixels);
+
+        BufferedImage image;
+        if (original.channels() > 1) {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        } else {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        }
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+
+        return image;
+    }
+}
